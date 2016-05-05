7652cf8ba946d4b1f9b40be599b0758e5793d9dc
ringtail003 <ringtail003@gmail.com>
2016-05-05 11:19:02 +0900

    とりあえずコピーボタン配置

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,49 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var $element = $('<button class="btn btn-default btn-sm">copy</button>');
+$('.gh-header-title').prepend($element);
+
+$element.on('click', () => {
+  let generator = new LinkGenerator();
+  let clipboard = new Clipboard();
+  clipboard.copy(generator.generate());
+});
+
+class LinkGenerator{
+  generate(){
+    return this.format(
+        this.readNumber(),
+        window.location.href,
+        this.readTitle()
+    );
+  }
+  format(number,url,title){
+    return `[${number.replace("#","")}](${url}) ${title}`;
+  }
+  readTitle(){
+    return $('.js-issue-title').text();
+  }
+  readNumber(){
+    return $('.gh-header-number').text();
+  }
+}
+
+class Clipboard{
+  addHiddenElement(text,container){
+    let $element = $(`<textarea>${text}</textarea>`);
+    $element.css('display','hidden');
+    $(container || 'body').append($element);
+
+    return $element; 
+  }
+  removeElement($element){
+    $element.remove();
+  }
+  copy(text){
+    let $element = this.addHiddenElement(text);
+    $element.select();
+    document.execCommand("copy");
+    this.removeElement($element);
+  }
+}
+
